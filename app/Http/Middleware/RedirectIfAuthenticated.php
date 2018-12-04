2c392329fe02f1b6d2cc802c91ac0f6049466b58
Juan Farias <dientuki@gmail.com>
2018-12-04 14:20:10 -0300

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,12 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            // Get the currently authenticated user...
+            if (Auth::user()->rol == 1){
+                return redirect('/admin/dashboard.html');
+            } else {
+                return redirect('/business/dashboard.html)';
+            }
         }
 
         return $next($request);
